a305a7f754e4f56c206ec680368a380c88c166ad
Gaston Sandstig <sandstig@student.chalmers.se>
2023-09-08 17:15:19 +0200

    remove circular preload of packedscenes

--- LevelComplete.gd
+++ LevelComplete.gd
@@ -1,11 +1,8 @@
 extends Node
 
-
-@export var startmenu: PackedScene
-
 func _ready():
 	GlobalTimer.stop()
 
 func _on_button_pressed():
-	get_tree().change_scene_to_packed(startmenu)
+	get_tree().change_scene_to_packed(load("res://LevelSelect.tscn"))
 	GlobalTimer.reset()
